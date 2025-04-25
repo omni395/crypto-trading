a730fa68e17e2fcbbfdf6a2fa1677fbc690d4b18
omni395 <omni395@gmail.com>
2025-04-25 11:50:23 +0300

    .

--- app/services/binance_cryptocurrencies_fetcher.rb
+++ app/services/binance_cryptocurrencies_fetcher.rb
@@ -10,18 +10,33 @@ class BinanceCryptocurrenciesFetcher
     uri = URI(API_URL)
     response = Net::HTTP.get(uri)
     data = JSON.parse(response)
+
+    # Получаем текущие монеты из базы
+    db_cryptos = Cryptocurrency.where(market_type: 'spot').pluck(:symbol, :base_asset, :quote_asset, :status).index_by { |s, _, _, _| s }
+    api_symbols = []
+
     data.each do |item|
       symbol = item['symbol']
       base_asset = item['symbol'].sub(/#{item['quoteAsset']}\z/, '')
       quote_asset = item['quoteAsset']
-      Cryptocurrency.find_or_initialize_by(symbol: symbol, market_type: 'spot').tap do |crypto|
-        crypto.base_asset = base_asset
-        crypto.quote_asset = quote_asset
-        crypto.status = 'active' # Можно доработать по API
-        crypto.market_type = 'spot'
-        crypto.save!
+      status = 'active' # Можно доработать по API
+      api_symbols << symbol
+
+      db_crypto = db_cryptos[symbol]
+      if db_crypto.nil?
+        # Новая монета
+        Cryptocurrency.create!(symbol: symbol, base_asset: base_asset, quote_asset: quote_asset, status: status, market_type: 'spot')
+      else
+        # Обновляем только если что-то изменилось
+        db_base, db_quote, db_status = db_crypto[1], db_crypto[2], db_crypto[3]
+        if db_base != base_asset || db_quote != quote_asset || db_status != status
+          Cryptocurrency.where(symbol: symbol, market_type: 'spot').update_all(base_asset: base_asset, quote_asset: quote_asset, status: status)
+        end
       end
     end
+
+    # Деактивируем монеты, которых больше нет в API
+    Cryptocurrency.where(market_type: 'spot').where.not(symbol: api_symbols).update_all(status: 'inactive')
     Rails.logger.info "[CryptoListing] Finish update at #{Time.current}"
   end
 end
--- app/services/binance_futures_cryptocurrencies_fetcher.rb
+++ app/services/binance_futures_cryptocurrencies_fetcher.rb
@@ -10,18 +10,29 @@ class BinanceFuturesCryptocurrenciesFetcher
     uri = URI(API_URL)
     response = Net::HTTP.get(uri)
     data = JSON.parse(response)
+
+    db_cryptos = Cryptocurrency.where(market_type: 'futures').pluck(:symbol, :base_asset, :quote_asset, :status).index_by { |s, _, _, _| s }
+    api_symbols = []
+
     data.each do |item|
       symbol = item['symbol']
       base_asset = item['symbol'].sub(/#{item['quoteAsset']}\z/, '')
       quote_asset = item['quoteAsset']
-      Cryptocurrency.find_or_initialize_by(symbol: symbol, market_type: 'futures').tap do |crypto|
-        crypto.base_asset = base_asset
-        crypto.quote_asset = quote_asset
-        crypto.status = 'active' # Можно доработать по API
-        crypto.market_type = 'futures'
-        crypto.save!
+      status = 'active' # Можно доработать по API
+      api_symbols << symbol
+
+      db_crypto = db_cryptos[symbol]
+      if db_crypto.nil?
+        Cryptocurrency.create!(symbol: symbol, base_asset: base_asset, quote_asset: quote_asset, status: status, market_type: 'futures')
+      else
+        db_base, db_quote, db_status = db_crypto[1], db_crypto[2], db_crypto[3]
+        if db_base != base_asset || db_quote != quote_asset || db_status != status
+          Cryptocurrency.where(symbol: symbol, market_type: 'futures').update_all(base_asset: base_asset, quote_asset: quote_asset, status: status)
+        end
       end
     end
+
+    Cryptocurrency.where(market_type: 'futures').where.not(symbol: api_symbols).update_all(status: 'inactive')
     Rails.logger.info "[CryptoListing:Futures] Finish update at #{Time.current}"
   end
 end
